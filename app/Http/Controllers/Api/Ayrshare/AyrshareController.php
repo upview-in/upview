e5162c61ce19bda5f015a9a41d02e8e7cf1b3a27
spandy1 <spandandholakia@gmail.com>
2021-12-24 12:05:10 +0530

    Git directory corruption restoration

---
+++ app/Http/Controllers/Api/Ayrshare/AyrshareController.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Http\Controllers\Api\Ayrshare;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Api\Ayrshare\AyrCreateProfile;
+use App\Http\Requests\Api\Ayrshare\AyrDeleteProfile;
+use App\Http\Requests\Api\Ayrshare\AyrPostAnalytics;
+use App\Http\Requests\Api\Ayrshare\AyrShortLinkAnalysis;
+use App\Http\Requests\Api\Ayrshare\AyrSocialMediaPosts;
+use Illuminate\Support\Facades\Http;
+
+
+class AyrshareController extends Controller
+{
+    public static function ayrshareAPICall($method, $endpoint, $body)
+    {
+        if (!strcmp($method, "GET")) {
+            $response = Http::withHeaders([
+                'Authorization' => 'Bearer '.config('ayrshare.AYR_API_KEY'),
+            ])->withOptions(['verify' => true])->get($endpoint, $body);
+        } else {
+            $response = Http::withHeaders([
+                'Authorization' => 'Bearer '.config('ayrshare.AYR_API_KEY'),
+            ])->withOptions(['verify' => true])->post($endpoint, $body);
+        }
+        return $response;
+    }
+
+    public function createAyrProfile(AyrCreateProfile $request)
+    {
+        return AyrshareController::ayrshareAPICall('POST', config('ayrshare.AYR_CREATE_PROFILE_ENDPOINT'), ['title' => $request->email]);
+    }
+
+
+    public function deleteAyrProfile(AyrDeleteProfile $request)
+    {
+        return AyrshareController::ayrshareAPICall('POST', config('ayrshare.AYR_DELETE_PROFILE_ENDPOINT'), ['profileKey' => $request->profile_key]);
+    }
+
+    public function ayrShortLinkAnalytics(AyrShortLinkAnalysis  $request)
+    {
+        return AyrshareController::ayrshareAPICall('GET', config('ayrshare.AYR_SHORT_LINK_ANALYTICS_ENDPOINT'), ['lastDays' => $request->lastDays]);
+    }
+
+
+    public function ayrPostAnalytics(AyrPostAnalytics $request)
+    {
+        return AyrshareController::ayrshareAPICall('GET', config('ayrshare.AYR_SHORT_LINK_ANALYTICS_ENDPOINT'), ['id' => $request->post_id, 'platforms' => $request->platforms]);
+    }
+
+    public function ayrSocialMediaPlatformAnalytics(AyrPostAnalytics $request)
+    {
+        return AyrshareController::ayrshareAPICall('GET', config('ayrshare.AYR_SOCIAL_MEDIA_PLATFORM_ANALYTICS_ENDPOINT'), ['platforms' => $request->platforms]);
+    }
+
+    public function ayrSocialMediaPosts(AyrSocialMediaPosts $request)
+    {
+        /**
+         * instagramOptions": {
+         *       "autoResize": true/false
+         *       "locationId": FB Location's Page
+         *       "userTags": [
+         *           {
+         *               "username": "instauser"
+         *               "x":
+         *               "y": x & y for tag location
+         *           },
+         *           {
+         *               "username": "instauser2"
+         *               "x":
+         *               "y":
+         *          }
+         *       ]
+         *   }
+         */
+
+        if ($request->has('post') && $request->has('platforms')) {
+            return AyrshareController::ayrshareAPICall(
+                'GET',
+                config('ayrshare.AYR_MEDIA_POST_ENDPOINT'),
+                [
+                    'post' => $request->caption,
+                    'platforms' => $request->platforms,
+                    'mediaURLs' => $request->mediaURLs ?? [''],
+                    'instagramOptions' => $request->instagramOptions ?? [''],
+                ]
+            );
+        }
+    }
+
+    public function showAccountLinkOptions()
+    {
+            /*
+                ok
+            */
+    }
+
+
+
+    public function index()
+    {
+        /**
+         * Call the function you wanna test
+         */
+    }
+}
