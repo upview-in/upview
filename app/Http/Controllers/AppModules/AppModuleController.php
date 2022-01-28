5600b9ae7729f916293589e6fb2385db673eaec3
hacker7599 <psuman012@gmail.com>
2022-01-28 10:10:40 +0530

    Ayrshare Modification and Privacy Policy Update ** Added logout parameter in ayrshare profile managment to force logout ** Added Numbering to left over headings in Privacy Policy

--- app/Http/Controllers/AppModules/AppModuleController.php
+++ app/Http/Controllers/AppModules/AppModuleController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers\AppModules;
 
 use App\Http\Controllers\Controller;
+use UserPermissionHelper;
 
 class AppModuleController extends Controller
 {
     public function index()
     {
+        $packages = (new UserPermissionHelper)->getGroups();
+        dd($packages);
         return view('user.site_packages');
     }
 }
