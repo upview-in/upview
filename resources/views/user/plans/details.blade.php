ee5571e7c579df1c25966943ea2dfe3997113896
jd patel <jaydeepukani@outlook.in>
2022-03-31 21:21:32 +0530

    Added payments mangement to admin

--- resources/views/user/plans/details.blade.php
+++ resources/views/user/plans/details.blade.php
@@ -19,7 +19,11 @@
                 <p>{{ $plan->shortDescription }}</p>
                 <label class="font-weight-bolder" style="font-size: large;">${{ $plan->price }}</label>
                 <br>
-                <a href="{{ route('panel.user.plans.buy', $plan->id) }}" class="btn btn-primary">Buy now</a>
+                @if (in_array($plan->id, $purchased_plans))
+                    <label class="font-weight-bolder text-success">Plan is Activated</label>
+                @else
+                    <a href="{{ route('panel.user.plans.buy', $plan->id) }}" class="btn btn-primary">Buy now</a>
+                @endif
                 <div class="mt-5">
                     {!! $plan->longDescription !!}
                 </div>
