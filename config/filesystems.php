8a86a92629260b9dca551a3b25c33ec9332dc507
jd patel <jaydeepukani@outlook.in>
2022-04-05 18:43:36 +0530

    Revert config/filesystem.php changes

--- config/filesystems.php
+++ config/filesystems.php
@@ -38,7 +38,7 @@ return [
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => (request()->secure() ? 'https' : 'http') . '://' . request()->getHost() . '/storage',
+            'url' => env('APP_URL') . '/storage',
             'visibility' => 'public',
         ],
 
