14426b78ac8fed5abde8ba908c8a3f7f5c8d1aa0
hacker7599 <psuman012@gmail.com>
2022-06-09 17:57:10 +0530

    Minor main site modification (#109)
    **Modified CSS for about section
    **Uploaded Terms & Condition page
    **Modified controller and routes for a new page
    **Modified css file to change the responsive design

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -4,7 +4,7 @@
 
         <p class="m-b-0">Copyright &copy; {{ \Carbon\Carbon::now()->year . ' ' . Config::get('app.name')}}. All rights reserved.</p>
         <span>
-            <a href="" class="text-gray m-r-15">Term &amp; Conditions</a>
+            <a href="{{ route('main.terms-conditions') }}" class="text-gray m-r-15">Term &amp; Conditions</a>
             <a href="{{ route('main.privacy-policy') }}" target="_blank" class="text-gray">Privacy &amp; Policy</a>
         </span>
     </div>
