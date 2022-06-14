5c159c92e664f5b3f4d0b58f9b87ae322bb6fc43
jd patel <jaydeepukani@outlook.in>
2022-06-14 21:57:34 +0530

    Added popup for expired plan

--- app/Http/Controllers/User/PlansController.php
+++ app/Http/Controllers/User/PlansController.php
@@ -79,4 +79,63 @@ class PlansController extends Controller
             Auth::user()->roles()->sync(array_diff($role_ids, $expired_role_ids));
         }
     }
+
+    public static function getMessage(): array
+    {
+        $is_any_plans_are_active = false;
+        $timeout = 0;
+        $display = false;
+        $message = 'Your ';
+        $expired_plans = $expired_soon_plans = [];
+        $active_orders = UserOrder::with('plan')->where('user_id', Auth::id())->whereIn('status', [1, 4])->get();
+
+        foreach ($active_orders as $order) {
+            if (!empty($order->plan)) {
+                if ($order->status == 4 && Carbon::now()->gte($order->expired_at)) {
+                    $display = true;
+                    $expired_plans[] = $order->plan->name;
+                } elseif ($order->status == 1 && Carbon::now()->gte($order->expired_at->subDays(7))) {
+                    $display = true;
+                    $expired_soon_plans[] = $order->plan->name;
+                } else {
+                    $is_any_plans_are_active = true;
+                }
+            }
+        }
+
+        $expired_plans = array_unique($expired_plans);
+        $expired_soon_plans = array_unique($expired_soon_plans);
+
+        if (count($expired_soon_plans) > 1) {
+            $expired_soon_plans[count($expired_soon_plans) - 1] = 'and ' . $expired_soon_plans[count($expired_soon_plans) - 1];
+            $message .= implode(', ', $expired_soon_plans) . ' plans will be expired in 7 days.';
+        } else {
+            if (!empty($expired_soon_plans)) {
+                $message .= $expired_soon_plans[0] . ' plan will be expired in 7 days.';
+            }
+        }
+
+        $message .= ' ';
+
+        if (count($expired_plans) > 1) {
+            $expired_plans[count($expired_plans) - 1] = 'and ' . $expired_plans[count($expired_plans) - 1];
+            $message .= implode(', ', $expired_plans) . ' plans are expired.';
+        } else {
+            if (!empty($expired_plans)) {
+                $message .= $expired_plans[0] . ' plan is expired.';
+            }
+        }
+
+        $message .= ' <a href="' . route('panel.user.plans.list') . '">Active now!</a>';
+
+        if ($is_any_plans_are_active) {
+            $timeout = 30;
+        }
+
+        return [
+            'display' => $display,
+            'message' => $message,
+            'timeout' => $timeout
+        ];
+    }
 }
