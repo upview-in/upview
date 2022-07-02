5516994d814508faa872c1a8544ce164ce9b9308
jd patel <jaydeepukani@outlook.in>
2022-07-02 12:06:23 +0530

    Fixed migrate issue

--- database/migrations/2022_06_22_201648_create_sms_logs_table.php
+++ database/migrations/2022_06_22_201648_create_sms_logs_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateSmsLogTable extends Migration
+class CreateSmsLogsTable extends Migration
 {
     /**
      * Run the migrations.
