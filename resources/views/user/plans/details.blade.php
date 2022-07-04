848e1eb91d5b241b30e001e80038ab02691700db
jd patel <jaydeepukani@outlook.in>
2022-07-04 19:12:24 +0530

    Changed USD to INR and hide Stripe PG

--- resources/views/user/plans/details.blade.php
+++ resources/views/user/plans/details.blade.php
@@ -17,13 +17,13 @@
             <div class="col-12">
                 <h1>{{ $plan->name }}</h1>
                 <p>{!! $plan->shortDescription !!}</p>
-                <label class="font-weight-bolder" style="font-size: large;">${{ $plan->price }}</label>
+                <label class="font-weight-bolder" style="font-size: large;">Rs. {{ $plan->price }}</label>
                 <br>
                 @if (in_array($plan->id, $purchased_plans))
                     <label class="font-weight-bolder text-success">Plan is Activated</label>
                 @else
-                    <a href="{{ route('panel.user.plans.buy', ['plan' => $plan->id, 'paymentGateway' => 0]) }}" class="btn btn-primary">Buy now</a>
-                    <a href="{{ route('panel.user.plans.buy', ['plan' => $plan->id, 'paymentGateway' => 1]) }}" class="btn btn-warning">Buy now (India)</a>
+                    {{-- <a href="{{ route('panel.user.plans.buy', ['plan' => $plan->id, 'paymentGateway' => 0]) }}" class="btn btn-primary">Buy now</a> --}}
+                    <a href="{{ route('panel.user.plans.buy', ['plan' => $plan->id, 'paymentGateway' => 1]) }}" class="btn btn-warning">Buy now</a>
                 @endif
                 <div class="mt-5">
                     {!! $plan->longDescription !!}
