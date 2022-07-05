c5f5241dfcbfbcd206e34e7bdd131916579fbedc
spandy1 <spandandholakia@gmail.com>
2022-07-05 13:25:32 +0530

    Instagram Overview Revamp Backup

--- app/Http/Controllers/Api/Instagram/InstagramController.php
+++ app/Http/Controllers/Api/Instagram/InstagramController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Api\Instagram;
 
+use App\Helper\FacebookHelper;
 use App\Helper\InstagramHelper;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\Instagram\Account\GetMineAccountDetails;
@@ -11,14 +12,57 @@ use Illuminate\Support\Arr;
 
 class InstagramController extends Controller
 {
-    public static function listInstaBusinessAccountsData()
+    public static function listFBPages()
+    {
+        $fb = new FacebookHelper();
+        $fb_client = $fb->getFacebookClient();
+        $fbUser = $fb_client->get('/me/accounts')->getBody();
+        $fbUser = json_decode($fbUser, true);
+        $totalPages = count(($fbUser['data']));
+        $dataArr = [];
+
+        for ($i = 0; $i < $totalPages; $i++) {
+            $fbPageID = $fbUser['data'][$i]['id'];
+            $fbData = $fb_client->get($fbPageID . '?fields=name,picture')->getGraphUser();
+
+            $dataArr[$i]['id'] = $fbPageID;
+            $dataArr[$i]['name'] = $fbData['name'];
+            $dataArr[$i]['picture'] = $fbData['picture']['url'];
+            $fbData = $fb_client->get('/me?fields=email')->getGraphUser();
+            $dataArr[$i]['email'] = $fbData['email'];
+        }
+        return $dataArr;
+    }
+
+    public static function getInstagramAccountForPage($pageID)
     {
         $ig = new InstagramHelper();
         $ig_client = $ig->getInstagramClient();
 
-        $IGUser = $ig_client->get('/me/accounts?fields=instagram_business_account')->getGraphEdge();
+        $IGUser = $ig_client->get($pageID.'?fields=instagram_business_account')->getGraphUser();
         dd($IGUser);
-        // $IGUser = $ig_client->get('/' . $MINEUserID[session('AccountIndex_IG', 0)]['id'] . '?fields=instagram_business_account')->getGraphUser();
+    }
+    public static function listInstaBusinessAccountsData()
+    {
+        $fbPages = InstagramController::listFBPages();
+        $ig = new InstagramHelper();
+        $ig_client = $ig->getInstagramClient();
+
+        $dataArr = [];
+
+        foreach($fbPages as $fbPage)
+        {
+            $token =   $ig_client->get('/' . $fbPage['id'] . '?fields=access_token')->getGraphUser();
+            $ig_client->setDefaultAccessToken($token['access_token']);
+            $IGUser = $ig_client->get($fbPage['id'].'/accounts?fields=instagram_accounts')->getGraphEdge();
+            foreach($IGUser as $ig)
+            {
+                $dataArr[] = $ig_client->get('/'.$ig->id.'?fields=id,,name')->getGraphEdge();
+            }
+        }
+
+
+         dd($dataArr);
     }
 
     public function getMineAccountInsights(GetMineAccountDetails $request)
@@ -26,23 +70,10 @@ class InstagramController extends Controller
         $ig = new InstagramHelper();
         $ig_client = $ig->getInstagramClient();
 
-        $IGUser = $ig_client->get('/me/accounts?fields=instagram_business_account')->getGraphEdge();
-        // $IGUser = $ig_client->get('/' . $MINEUserID[session('AccountIndex_IG', 0)]['id'] . '?fields=instagram_business_account')->getGraphUser();
-
-        $key = 0;
-        $IGBizAcc = [];
-        foreach ($IGUser as $acc) {
-            if (Arr::exists($acc, 'instagram_business_account')) {
-                $IGBizAcc[$key++] = $acc['instagram_business_account']['id'];
-            }
-        }
-        $key = 0;
-        foreach ($IGBizAcc as $acc=>$id) {
-            $IGData = $ig_client->get('/' . $MINE . '/insights?metric=' . $request->fields . '&period=days_28')->getBody();
-        }
-        $IGData = $ig_client->get('/' . $MINE . '/insights?metric=' . $request->fields . '&period=days_28')->getBody();
-
-        return response()->json(json_decode($igUser, true), 200);
+        $IGData = $ig_client->get('/'.$request->id . '?fields=followers_count,follows_count,id,name,media_count,profile_picture_url,username')->getGraphUser();
+        
+        dd($IGData);
+        return response()->json(json_decode($IGData, true), 200);
     }
 
     public static function getMineAccountInsightsEx($period = 'days_28')
@@ -110,19 +141,6 @@ class InstagramController extends Controller
         }
 
         return $sum;
-    }
-
-    public function getMineAccountData(GetMineAccountDetails $request)
-    {
-        $ig = new InstagramHelper();
-        $ig_client = $ig->getInstagramClient();
-
-        $MINEUserID = $ig_client->get('/me/accounts')->getGraphEdge();
-        $MINEUserID = $ig_client->get('/' . $MINEUserID[0]['id'] . '?fields=instagram_business_account')->getGraphUser();
-        $MINE = $MINEUserID['instagram_business_account']['id'];
-        $igUser = $ig_client->get('/' . $MINE . '?fields=' . $request->fields)->getBody();
-
-        return response()->json(json_decode($igUser, true), 200);
     }
 
     // @TODO Find some workaround to get Instagram Verified account status
